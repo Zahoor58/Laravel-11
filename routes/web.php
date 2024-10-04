0c934bcb3954dad565a484f3956ed24c2e42425f
Zahoor Ahmad <86906265+Zahoor58@users.noreply.github.com>
2024-10-05 03:42:57 +0500

    some fun with editing, updating and deleting a resource

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Models\Job;
 Route::get('/', function () {
     return view('home');
 });
-
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
     return view('jobs.index', [
@@ -21,14 +21,19 @@ Route::get("/contact", function () {
     return view("contact");
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
+
+// Show
 Route::get("/jobs/{id}", function ($id) {
 
     $job = Job::find($id);
     return view("jobs.show", ['job' => $job]);
 });
+
+// Store
 Route::post('jobs', function () {
     request()->validate([
         'title' => ['required',' min: 3', 'max: 25'],
@@ -40,5 +45,50 @@ Route::post('jobs', function () {
         'salary' => request('salary'),
         'employer_id' => 1
     ]);
+    return redirect('/jobs');
+});
+
+
+// Edit
+Route::get("/jobs/{id}/edit", function ($id) {
+
+    $job = Job::find($id);
+    return view("jobs.edit", ['job' => $job]);
+});
+
+// Update
+Route::patch("/jobs/{id}", function ($id) {
+
+    // Validate
+    request()->validate([
+        'title' => ['required',' min: 3', 'max: 25'],
+        'salary'=> ['required'],
+    ]);
+    // Autherize (on held..)
+
+    // Update the job
+    // First method
+    // $job = Job::find($id);
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    //Second method
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+    // redirect to th job page
+    return redirect("/jobs/{$job->id}");
+});
+
+
+// Destroy
+Route::delete("/jobs/{id}", function ($id) {
+    // Autherize (on held..)
+    // Delete the job
+    $job = Job::findOrFail($id)->delete();
+    // redirect to th job page
     return redirect('/jobs');
 });
